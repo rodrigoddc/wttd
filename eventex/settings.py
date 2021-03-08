881818cc01004788c908703980a48839f1ada314
rodrigo <rodrigoddc1@gmail.com>
2021-03-08 11:54:35 -0300

    DONE: added django-extenions and jupyterlab; splited requirements.text to -dev.txt

--- eventex/settings.py
+++ eventex/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third apps
+    'django_extensions',
+
+    # my apps
     'eventex.core',
     'eventex.subscriptions',
 ]
