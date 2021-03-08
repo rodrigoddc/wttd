b096c744f4a3e6374fe89f0c614959e961ee0a77
rodrigo <rodrigoddc1@gmail.com>
2021-03-07 22:59:44 -0300

    DONE: added custom errors pages

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -58,13 +58,13 @@ class SubscribePostTest(TestCase):
 
 	def test_subscription_email_from(self):
 		email = mail.outbox[0]
-		expect = 'contato@eventext.com'
+		expect = 'rodrigoddc1@gmail.com'
 
 		self.assertEqual(expect, email.from_email)
 
 	def test_subscription_email_to(self):
 		email = mail.outbox[0]
-		expect = ['contato@eventex.com.br', 'a@a.com']
+		expect = ['rodrigoddc1@gmail.com', 'a@a.com']
 
 		self.assertEqual(expect, email.recipients())
 
