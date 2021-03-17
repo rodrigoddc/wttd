5e008760fae07d2998306decce95184589b37572
rodrigo <rodrigoddc1@gmail.com>
2021-03-17 20:16:03 -0300

    DONE: added speakers keynotes

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -20,3 +20,27 @@ class HomeTest(TestCase):
 		""" GET / must contain subscription link """
 		expected = f"""href="{resolve_url('subscriptions:new')}"""
 		self.assertContains(self.response, expected)
+
+	def test_speakers(self):
+		""" GET / must show keynotes speakers """
+		contents = [
+			'Grace Hopper',
+			'http://hbn.link/hopper-pic',
+			'Alan Turing',
+			'http://hbn.link/turing-pic'
+		]
+		for expected in contents:
+			with self.subTest():
+				self.assertContains(self.response, expected)
+
+	def test_nav_links(self):
+		contents =[
+			f'href="{resolve_url("home")}#overview"',
+			f'href="{resolve_url("home")}#speakers"',
+			f'href="{resolve_url("home")}#sponsors"',
+			f'href="{resolve_url("home")}#register"',
+			f'href="{resolve_url("home")}#venue"',
+		]
+		for expected in contents:
+			with self.subTest():
+				self.assertContains(self.response, expected)
